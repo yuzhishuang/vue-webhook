f4ce539e0a445ac78ca5d67a9f3d010df7216df2
yuzhishuang <yuzhishuang@100tal.com>
2019-09-26 14:14:15 +0800

    增加vue-front版本号

--- vue-front.sh
+++ vue-front.sh
@@ -9,9 +9,9 @@ git pull origin master
 echo "打包最新代码"
 npm run build
 echo "开始构建镜像"
-docker build -t vue-front .
+docker build -t vue-front:1.0 .
 echo "删除旧容器"
 docker stop vue-front-container
 docker rm vue-front-container
 echo "启动新容器"
-docker container run -p 80:80 -d --name vue-front-container vue-front
+docker container run -p 80:80 -d --name vue-front-container vue-front:1.0
